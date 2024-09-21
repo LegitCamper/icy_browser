3d2434d830fab2b830116e1d768ed47e003000b2
sawyer bristol <sawyerbristol@gmail.com>
2024-09-21 17:40:02 -0600

    working on build.rs

---
+++ build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // ensure runtime resources exist
+    #[cfg(feature = "ultralight")]
+    {
+        let path = env!("CARGO_MANIFEST_DIR");
+
+        let possible_directories = Vec::new();
+    }
+}
